2ebcbd58be5058618fbec991f91165d6f385c2fb
Sonia Connolly <sconnolly@pluribusdigital.com>
2024-01-26 14:58:41 -0800

    Add author-only search builder param

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -4,9 +4,13 @@ class SearchBuilder < Blacklight::SearchBuilder
 
   ##
   # @example Adding a new step to the processor chain
-  #   self.default_processor_chain += [:add_custom_data_to_query]
-  #
-  #   def add_custom_data_to_query(solr_parameters)
-  #     solr_parameters[:custom] = blacklight_params[:user_value]
-  #   end
+  self.default_processor_chain += [:add_author_only_qf]
+
+  def add_custom_data_to_query(solr_parameters)
+    solr_parameters[:custom] = blacklight_params[:user_value]
+  end
+
+  def add_author_only_qf(solr_parameters)
+    solr_parameters[:qf] = 'author_tsim' if blacklight_params[:author]
+  end
 end
