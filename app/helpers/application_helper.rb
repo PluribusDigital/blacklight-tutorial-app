a990830aa9196d5124807fca5999f68c5962d25a
Sonia Connolly <sconnolly@pluribusdigital.com>
2024-01-26 15:07:58 -0800

    thumbnails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_thumbnail(document, options)
+    return unless document[:file_id].present?
+    image_tag(
+      "#{image_server}/#{document.id}/#{document.first(:file_id)}.png",
+      options.merge(alt: presenter(document).document_heading)
+    )
+  end
 end
